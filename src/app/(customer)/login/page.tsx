267995a80a42f09e1693f299154b61ec79bbfe5f
Matheus Borges <matheusb.dev@gmail.com>
2026-02-23 18:36:56 -0300

    feat: add background images to login and register pages

--- src/app/(customer)/login/page.tsx
+++ src/app/(customer)/login/page.tsx
@@ -2,8 +2,12 @@ import { UserLoginForm } from '@/features/auth/components/user-login-form';
 
 export default function LoginPage() {
   return (
-    <div className="bg-background from-background to-background relative flex min-h-screen items-center justify-center via-zinc-800 px-4 dark:bg-linear-to-br">
-      <UserLoginForm />
+    <div className="grid grid-cols-[1fr_500px]">
+      <div className="bg-[url('/login-img.jpg')] bg-cover bg-center" />
+
+      <div className="bg-background from-background to-background relative flex min-h-screen items-center justify-center via-zinc-800 px-4 dark:bg-linear-to-br">
+        <UserLoginForm />
+      </div>
     </div>
   );
 }
--- src/app/(customer)/register/page.tsx
+++ src/app/(customer)/register/page.tsx
@@ -2,8 +2,11 @@ import { RegisterForm } from '@/features/auth/components/register-form';
 
 export default function RegisterPage() {
   return (
-    <div className="bg-background from-background to-background relative flex min-h-screen items-center justify-center via-zinc-800 px-4 dark:bg-linear-to-br">
-      <RegisterForm />
+    <div className="grid grid-cols-[1fr_500px]">
+      <div className="bg-[url('/register-img.jpg')] bg-cover bg-center" />
+      <div className="bg-background from-background to-background relative flex min-h-screen items-center justify-center via-zinc-800 px-4 dark:bg-linear-to-br">
+        <RegisterForm />
+      </div>
     </div>
   );
 }
